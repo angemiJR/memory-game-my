9d01608a0806c17d034744210da546234e06b078
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-02-04 15:37:33 +0100

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Navbar from "./Navbar.jsx";
-// import Login from "./pages/Login.jsx";
 import Game from "./pages/Game.jsx";
-// import UserInfo from "./pages/User-info.jsx";
 import SignUp from "./pages/Sign-up.jsx";
 import PlayGamePage from "./pages/PlayGamePage.jsx";
 import Login from "./pages/Login.jsx";
 
 function App() {
   const location = useLocation(); // Get the current route
+  const [isPortrait, setIsPortrait] = useState(window.innerHeight > window.innerWidth);
+
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      setIsPortrait(window.innerHeight > window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleOrientationChange);
+    return () => window.removeEventListener("resize", handleOrientationChange);
+  }, []);
+
+  // If in portrait mode, show a message instead of the app
+  if (isPortrait) {
+    return (
+      <div className="landscape">
+       <h2>Please rotate your device to landscape mode.</h2> 
+      </div>
+    );
+  }
 
   return (
     <>
-      
-      {location.pathname !== "/game" && <Navbar />}     {/* Render Navbar unless on /game  */}
-   
+      {location.pathname !== "/game" && <Navbar />} {/* Render Navbar unless on /game */}
       <Routes>
         <Route path="/" element={<PlayGamePage />} />
         <Route path="/game" element={<Game />} />
