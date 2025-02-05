3e47c360a0a54cb660c487376f0e6b307e4e10c4
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-02-05 13:10:18 +0100

    settings

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
+import './styles/Login.css';
+import './styles/Sign-up.css';
+import './styles/PlayGamePage.css';
+import './styles/Game.css';
+
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
