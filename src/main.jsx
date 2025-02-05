706fac9904babbe4c1d5ca052b319df5400d7be7
angemiJR <111733781+angemiJR@users.noreply.github.com>
2025-02-05 13:14:40 +0100

    settings

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './styles/Login.css';
 import './styles/Sign-up.css';
 import './styles/PlayGamePage.css';
 import './styles/Game.css';
+import './styles/Cards.css';
+import './styles/Fonts.css';
+import './styles/Navbar.css';
 
 import App from './App.jsx';
 
